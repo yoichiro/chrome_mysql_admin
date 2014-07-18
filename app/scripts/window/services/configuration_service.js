acd16cb0f168845318ee6a2099f028dbc7f0614b
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2014-07-18 20:14:06 +0900

    Added the configuration item to change the row count per page in Rows panel.

--- app/scripts/window/services/configuration_service.js
+++ app/scripts/window/services/configuration_service.js
@@ -1,32 +1,64 @@
 chromeMyAdmin.factory("configurationService", ["$rootScope", "$q", "Configurations", function($rootScope, $q, Configurations) {
     "use strict";
 
-    return {
-        getDatabaseInfoAutoUpdateSpan: function() {
-            var deferred = $q.defer();
-            chrome.storage.sync.get("configurations", function(items) {
-                var configurations = items.configurations || {};
-                var result =
-                        configurations[Configurations.DATABASE_INFO_AUTO_UPDATE_SPAN];
-                if (result) {
-                    deferred.resolve(result);
-                } else {
-                    deferred.resolve(
-                        Configurations.DEFAULT_DATABASE_INFO_AUTO_UPDATE_SPAN);
-                }
+    var configurationChangeListeners = [];
+
+    var getConfigurationValue = function(name, defaultValue) {
+        var deferred = $q.defer();
+        chrome.storage.sync.get("configurations", function(items) {
+            var configurations = items.configurations || {};
+            var result = configurations[name];
+            if (result) {
+                deferred.resolve(result);
+            } else {
+                deferred.resolve(defaultValue);
+            }
+        });
+        return deferred.promise;
+    };
+
+    var setConfigurationValue = function(name, value) {
+        var deferred = $q.defer();
+        chrome.storage.sync.get("configurations", function(items) {
+            var configurations = items.configurations || {};
+            configurations[name] = value;
+            chrome.storage.sync.set({configurations: configurations}, function() {
+                deferred.resolve();
+                notifyConfigurationChanged(name, value);
             });
-            return deferred.promise;
+        });
+        return deferred.promise;
+    };
+
+    var notifyConfigurationChanged = function(name, value) {
+        angular.forEach(configurationChangeListeners, function(listener) {
+            listener(name, value);
+        });
+    };
+
+    return {
+        addConfigurationChangeListener: function(listener) {
+            configurationChangeListeners.push(listener);
+        },
+        getDatabaseInfoAutoUpdateSpan: function() {
+            return getConfigurationValue(
+                Configurations.DATABASE_INFO_AUTO_UPDATE_SPAN,
+                Configurations.DEFAULT_DATABASE_INFO_AUTO_UPDATE_SPAN);
         },
         setDatabaseInfoAutoUpdateSpan: function(span) {
-            var deferred = $q.defer();
-            chrome.storage.sync.get("configurations", function(items) {
-                var configurations = items.configurations || {};
-                configurations[Configurations.DATABASE_INFO_AUTO_UPDATE_SPAN] = span;
-                chrome.storage.sync.set({configurations: configurations}, function() {
-                    deferred.resolve();
-                });
-            });
-            return deferred.promise;
+            return setConfigurationValue(
+                Configurations.DATABASE_INFO_AUTO_UPDATE_SPAN,
+                span);
+        },
+        getRowCountPerPageInRowsPanel: function() {
+            return getConfigurationValue(
+                Configurations.ROW_COUNT_PER_PAGE_IN_ROWS_PANEL,
+                Configurations.DEFAULT_ROW_COUNT_PER_PAGE_IN_ROWS_PANEL);
+        },
+        setRowCountPerPageInRowsPanel: function(rowCount) {
+            return setConfigurationValue(
+                Configurations.ROW_COUNT_PER_PAGE_IN_ROWS_PANEL,
+                rowCount);
         }
     };
 }]);
